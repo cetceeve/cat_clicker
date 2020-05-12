0e1828809baa6e05787a65388870710260ee4058
Fabian Zeiher <fzeiher@gmail.com>
2020-05-12 10:32:44 +0200

    enable click functionality

--- res/js/index.js
+++ res/js/index.js
@@ -1,1 +1,6 @@
 console.log("loaded");
+
+document.querySelector("main .pic").addEventListener("click", () => {
+    let value = document.querySelector("main .value").innerHTML;
+    document.querySelector("main .value").innerHTML = parseInt(value) + 1;
+});
