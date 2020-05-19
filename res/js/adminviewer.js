9362a019b6092ad53899c1d4584df0772f803df5
Fabian Zeiher <fzeiher@gmail.com>
2020-05-19 23:24:31 +0200

    finished admin viewer

--- res/js/adminviewer.js
+++ res/js/adminviewer.js
@@ -3,23 +3,52 @@ import * as _ from "../../vendors/underscore/underscore.js";
 class AdminViewer extends EventTarget {
     constructor() {
         super();
-        this.inputFieldTemplateCompiled = _.template(document.querySelector("#input_admin").innerHTML);
+        this.inputFieldTemplateCompiled = _.template(document.querySelector("#template_admin").innerHTML);
         this.ADMINVIEWEREL = document.querySelector("main .admin_viewer");
+        this.INPUTCONTAINEREL = this.ADMINVIEWEREL.querySelector(".input_admin");
+        this.ADMINBUTTONTEMPLATESTRING = '<button id="admin">Admin</button>'; // eslint-disable-line quotes
         this.addClickListener();
     }
 
-    saveChanges(obj) {
+    addClickListener() {
+        this.ADMINVIEWEREL.querySelector("#admin").addEventListener("click", () => {
+            super.dispatchEvent(new Event("onAdminButtonClicked"));
+        });
+    }
+
+    saveChanges() {
+        let obj = this.getInputData();
         super.dispatchEvent((function () {
             let e = new Event("onSaveChanges");
             e.data = obj;
             return e;
         })());
+        this.resetView();
     }
 
     render(cat) {
+        this.ADMINVIEWEREL.querySelector("#admin").disabled = true;
         let div = document.createElement("DIV");
         div.innerHTML = this.inputFieldTemplateCompiled(cat);
-        this.ADMINVIEWEREL.appendChild(div);
+
+        this.INPUTCONTAINEREL.innerHTML = "";
+        this.INPUTCONTAINEREL.appendChild(div);
+
+        div.querySelector("#admin_cancel").addEventListener("click", this.resetView.bind(this));
+        div.querySelector("#admin_save").addEventListener("click", this.saveChanges.bind(this));
+    }
+
+    getInputData() {
+        return {
+            "name": this.INPUTCONTAINEREL.querySelector("#input_name").value,
+            "src": this.INPUTCONTAINEREL.querySelector("#input_src").value,
+            "clickCount": this.INPUTCONTAINEREL.querySelector("#input_click_count").value
+        };
+    }
+
+    resetView() {
+        this.INPUTCONTAINEREL.innerHTML = "";
+        this.ADMINVIEWEREL.querySelector("#admin").disabled = false;
     }
 }
 
--- res/js/index.js
+++ res/js/index.js
@@ -2,12 +2,17 @@ const CATS = JSON.parse(document.querySelector("#cat_list").innerHTML);
 
 import catViewer from "./catviewer.js";
 import catDrawer from "./catdrawer.js";
+import adminViewer from "./adminviewer.js";
 
 function init() {
     catDrawer.render(CATS.catList);
     catDrawer.addEventListener("onItemClicked", (event) => displayCat(event.data));
+
     catViewer.render(CATS.currentCat);
     catViewer.addEventListener("onPicClicked", (event) => incrementCounter(event.data));
+
+    adminViewer.addEventListener("onSaveChanges", (event) => saveChanges(event.data));
+    adminViewer.addEventListener("onAdminButtonClicked", displayAdmin);
 }
 
 function incrementCounter(catID) {
@@ -16,10 +21,21 @@ function incrementCounter(catID) {
     catViewer.render(cat);
 }
 
+function saveChanges(data) {
+    let catIndex = CATS.catList.findIndex(item => item.id === parseInt(CATS.admin.id));
+    console.log(catIndex);
+    console.log(data);
+}
+
 function displayCat(catID) {
     let cat = CATS.catList.find(item => item.id === parseInt(catID));
     CATS.currentCat = cat;
     catViewer.render(cat);
+}
+
+function displayAdmin() {
+    CATS.admin = CATS.currentCat;
+    adminViewer.render(CATS.currentCat);
 }
 
 init();
