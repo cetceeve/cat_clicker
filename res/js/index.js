1eb673431afa5eb35bb941d58335f09cf6976282
Fabian Zeiher <fzeiher@gmail.com>
2020-05-19 17:11:09 +0200

    prepare for admin view

--- res/js/index.js
+++ res/js/index.js
@@ -1,25 +1,24 @@
-// import * as _ from "../../vendors/underscore/underscore.js";
-
 const CATS = JSON.parse(document.querySelector("#cat_list").innerHTML);
 
 import catViewer from "./catviewer.js";
 import catDrawer from "./catdrawer.js";
 
 function init() {
-    catDrawer.render(CATS);
+    catDrawer.render(CATS.catList);
     catDrawer.addEventListener("onItemClicked", (event) => displayCat(event.data));
-    catViewer.render(CATS[0]);
+    catViewer.render(CATS.currentCat);
     catViewer.addEventListener("onPicClicked", (event) => incrementCounter(event.data));
 }
 
 function incrementCounter(catID) {
-    let cat = CATS.find(item => item.id === parseInt(catID));
+    let cat = CATS.catList.find(item => item.id === parseInt(catID));
     cat.clickCount = cat.clickCount + 1;
     catViewer.render(cat);
 }
 
 function displayCat(catID) {
-    let cat = CATS.find(item => item.id === parseInt(catID));
+    let cat = CATS.catList.find(item => item.id === parseInt(catID));
+    CATS.currentCat = cat;
     catViewer.render(cat);
 }
 
